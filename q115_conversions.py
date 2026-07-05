c14537887e531f91783e8ae055379a6726722414
Johnny <johnnybrown0g@gmail.com>
2026-07-05 17:13:08 -0500

    Q1.15 conversion + testing refactor
    Logic now uses int values with python f"{int_value:016b/0x}" to represent hex/binary values. This is a massive improvement form my initial implementation which used strings (looking back this refactor could've been avoided with more thought into design). Added test cases and confirmed functionality.

--- q115_conversions.py
+++ q115_conversions.py
@@ -1,101 +1,81 @@
+"""
+q115_conversions.py 
+
+This file stores methods for converting between float values and Q1.15 format binary values
+
+For more on Q number formatting see: 
+    https://onlinedocs.microchip.com/oxy/GUID-70ACD6B0-A33F-4653-B192-8465EAD1FD98-en-US-11/GUID-B8396BB4-C5C6-4E10-A0DC-C32A4B031857.html
+    https://en.wikipedia.org/wiki/Q_(number_format)
+ 
+We are using Q1.15, meaning there is one sign bit (+-) and 15 fraction bits 
+Each fractional bit represents 2^-N, this means Q1.15 can represent -1.0 to +0.999969482 
+"""
 import math
 import numpy as np
 
-# This relies on Q number formatting
-# See: https://onlinedocs.microchip.com/oxy/GUID-70ACD6B0-A33F-4653-B192-8465EAD1FD98-en-US-11/GUID-B8396BB4-C5C6-4E10-A0DC-C32A4B031857.html
-# See also: https://en.wikipedia.org/wiki/Q_(number_format)
-# 
-# We are using Q1.15, meaning there is one sign bit (+-)
-# and 15 fraction bits (2^-1 (.5) --> 2^-15 (0.000030518)), (like sig figs)
-# This means we can represent -1.0 up to +0.999969482 
-# These functions convert between Q1.15 "decimal" to a float value
 
-# Python doesn't like integers starting with 0, so Q1.15 values will be formatted as strings
+def float_to_q115(float_in):
+    """
+    converts a float value to an integer value whose bits map to Q1.15 format
 
-# takes a float (python value) and map it to a Q1.15 bit-interpretation
-    # input: float (base 10)
-    # step 1: multiply by 2**15 (effectively L shift by 15 decimal points)
-    #           We now have an int value, which shoudl be between 32767 and -32768. 
-    # step 10: Repeatedly mod2 to find bits; store in array
-    #           output_binary = IntValue % 2 # output will be 1/0
-    # step 11:  check original input: negative? 2's Comp flip the values (very annoying)
-def float_to_q115(INfloat):
+    Args:
+        float_in: input float. Values greater than 1/-1 will be truncated to the nearest value
+        (e.g. 1.5 will be stored as 1.0, -3 will be stored as -1)
+
+    Returns:
+        int: an int value representing the float in Q1.15 format.
+    """
+
+    # "Left shift" input to be Q115 format
+    int_q115 = round(float_in * 2**15)
+
+    # filter to fit representable range
+    if (int_q115 > 32767):
+        int_q115 = 32767
+    elif (int_q115 < -32768):
+        int_q115 = -32768
     
-    # step 1: Left shift input, filter to cap values exceeding range
-    Lshift_IN = round(INfloat * 2**15)
-    # filter to fit range
-    if (Lshift_IN > 32767):
-        Lshift_IN = 32767
-    elif (Lshift_IN < -32768):
-        Lshift_IN = -32768
+    return (int_q115 & 0xFFFF)
 
-    # print(f"{(Lshift_IN):016b}")
 
-    # step 10: store int value in an array as 16 bits of signed binary
-    # Values are stored here in big-endian format
-    q115_Arr = []  
-    while abs(Lshift_IN) >= 1:
-        q115_Arr.append(int(Lshift_IN % 2))
-        Lshift_IN = int(Lshift_IN / 2)
-    if (len(q115_Arr) < 16):            # extend to 16 bits, if needed
-        bits_to_extend = 16 - len(q115_Arr)
-        for i in range(bits_to_extend):
-            q115_Arr.append(0)          # always extend zero (2sComp later if needed)
+def q115_to_float(int_q115):
+    """
+    converts an integer representing a Q1.15 value into a float
+
+    Args:
+        int_q115: an int in Q1.15 format binary
+
+    Returns:
+        float: a float representation of the Q1.15 value.
+
+    Raises: 
+        ____Error: if input value falls outside of the valid Q1.15 range [32767,-32768]
+    """
+    if f"{int_q115:016b}"[0] == '1':
+        if (int_q115 > 32768):
+            int_q115 = (int_q115 ^ 0xFFFF) +1
+        return (-(int_q115)/(2**15))
     
-    # step 11: convert value to two's comp negative, if value is negative
-    # (positive values will inherently be in the correct format)
-    if INfloat < 0:
-        Flip_TwosComp(q115_Arr)
-    
+    if (int_q115 > 32767):
+        int_q115 = 32767
 
-    # convert from list to string, flip back to little-Endian in the process
-    OUTq115 = ""
-    for i in range(len(q115_Arr)):
-        OUTq115 += str(q115_Arr.pop())
-    
-    return (OUTq115)
+    return (int_q115/(2**15))
 
 
-
-def q115_to_float(INq115):
-    str_INq115 = str(INq115)
-    magnVal = 0.0
-    for i in range(1,16,1):
-        if int(str_INq115[i]):
-            magnVal += 2**(-i)
-    if str_INq115[0] == '1':
-        magnVal -= 1.0
-    return magnVal
-
-
-# Big Endian Notation input!!!
-def Flip_TwosComp(InArr):
-    TwosCompArr = []
-    
-    # Negate
-    for i in range(len(InArr)):
-        TwosCompArr.insert(i, (1 - InArr[i])) # 1-1 is 0, 1-0 is 1; flips all values
-    
-    # Add one (to least significant digit)
-    for i in range(len(TwosCompArr)):
-        if TwosCompArr[i] == 0:     # find first non-1 value 
-            TwosCompArr[i] = 1      # flip it to 1 (simplified addition-carry)
-            InArr = TwosCompArr     # ensure method is in-place
-            return InArr            # (I could remove this line if I really wanted...)
-    # if all values of negated array are 1, input array was all zeros; return input array
-    return InArr
+def main():
+    for i in range(21):
+        j = i/20
+        if (j == q115_to_float(float_to_q115(j))):
+            print(f"=====Let's go: your code is working at least partially: {j} =====")
+            print()
+        else:
+            print(f"    Here is your starting float:                        {j}")
+            print(f"    Here is the binary that Q1.15 got turned into:      {float_to_q115(j):016b}")
+            k = q115_to_float(float_to_q115(j))
+            print(f"    Here is the float that Q1.15 got turned  into:      {k}")
+            print(f"    The difference is                                   {j - k}")
+            print()
 
 
-# QUICK TESTS
-for i in range(21):
-    j = i/20
-    if (j == q115_to_float(float_to_q115(j))):
-        print(f"=====Let's go: your code is working at least partially: {j} =====")
-        print()
-    else:
-        print(f"    Here is your starting float:                        {j}")
-        print(f"    Here is the binary that Q1.15 got turned into:      {float_to_q115(j)}")
-        k = q115_to_float(float_to_q115(j))
-        print(f"    Here is the float that Q1.15 got turned  into:      {k}")
-        print(f"    The difference is                                   {j - k}")
-        print()
+if __name__ == "__main__":
+    main()
--- tests/test_q115_conversions.py
+++ tests/test_q115_conversions.py
@@ -1,57 +1,51 @@
-from q115_conversions import Flip_TwosComp, q115_to_float, float_to_q115
+from q115_conversions import q115_to_float, float_to_q115
 
 # Three tests to validate Q115 conversion behavior
 
-def test_Flip_TwosComp():
-    # These are Big-Endian
-    testArray1 = [1,0,0,0] # +1 (should flip to binary -1 == 0xF)
-    testArray2 = [1,1,1,0] # +7 (should flip to binary -7 == 0x9)
-    testArray3 = [0,0,0,0] # should return 0x0
-    testArray4 = [1,1,1,1] # -1 (should return +1 == 0x1)
-
-
-    result1 = Flip_TwosComp(testArray1)
-    result2 = Flip_TwosComp(testArray2)
-    result3 = Flip_TwosComp(testArray3)
-    result4 = Flip_TwosComp(testArray4)
-
-
-
-    assert result1 == [1,1,1,1]
-    assert result2 == [1,0,0,1]
-    assert result3 == [0,0,0,0]
-    assert result4 == [1,0,0,0]
-
 
 def test_q115_to_float():
-    Q115_1 = '0111111111111111' # 0x7FFF (.99...)
-    Q115_2 = '1000000000000000' # 0x8000 (-1.0)
-    Q115_3 = '0100000000000000' # 0x4000 (.5)
-    Q115_4 = '1100000000000000' # 0xC000 (-0.5)
+    q115_1 = 0b0111111111111111     # 0x7FFF (.99...)
+    q115_2 = 0b1000000000000000     # 0x8000 (-1.0)
+    q115_3 = 0b0100000000000000     # 0x4000 (.5)
+    q115_4 = 0b1100000000000000     # 0xC000 (-0.5)
+    # TODO:  I could test cases where my Q1.15 is formatted wrong (too large/small)
+    # q115_5 = 0b11000000000000000    # 0x18000 (should truncate extra bit and match -1.0)
+    # q115_6 = 0b10111111111111111    # 0x17FFF (should truncate extra bit and match .99)
 
-    result1 = q115_to_float(Q115_1)
-    result2 = q115_to_float(Q115_2)
-    result3 = q115_to_float(Q115_3)
-    result4 = q115_to_float(Q115_4)
+    result1 = q115_to_float(q115_1)
+    result2 = q115_to_float(q115_2)
+    result3 = q115_to_float(q115_3)
+    result4 = q115_to_float(q115_4)
+    # result5 = q115_to_float(q115_5)
+    # result6 = q115_to_float(q115_6)
 
     assert result1 == 0.999969482421875
     assert result2 == -1.0
     assert result3 == 0.5
     assert result4 == -0.5
+    # assert result5 == -1.0
+    # assert result6 == 0.999969482421875
 
 
 def test_float_to_q115():
-    testFloat1 = 0.999969482
-    testFloat2 = -1.0
-    testFloat3 = .5
-    testFloat4 = -0.5
+    test_float_1 = 0.999969482
+    test_float_2 = -1.0
+    test_float_3 = .5
+    test_float_4 = -0.5
+    test_float_5 = 1.5      # (should shorten to .0.999969482)
+    test_float_6 = -3       # (should shorten to -1.0)
     
-    result1 = float_to_q115(testFloat1)
-    result2 = float_to_q115(testFloat2)
-    result3 = float_to_q115(testFloat3)
-    result4 = float_to_q115(testFloat4)
+    result1 = float_to_q115(test_float_1)
+    result2 = float_to_q115(test_float_2)
+    result3 = float_to_q115(test_float_3)
+    result4 = float_to_q115(test_float_4)
+    result5 = float_to_q115(test_float_5)
+    result6 = float_to_q115(test_float_6)
 
-    assert result1 == '0111111111111111'
-    assert result2 =='1000000000000000'
-    assert result3 == '0100000000000000'
-    assert result4 == '1100000000000000'
+    assert result1 == 0b0111111111111111
+    assert result2 ==0b1000000000000000
+    assert result3 == 0b0100000000000000
+    assert result4 == 0b1100000000000000
+    assert result5 == 0b0111111111111111
+    assert result6 == 0b1000000000000000
+
