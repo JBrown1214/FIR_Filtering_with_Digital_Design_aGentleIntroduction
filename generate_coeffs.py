c3cb6b9018eab4182a6d961bb99650adbbe52bbb
Johnny <johnnybrown0g@gmail.com>
2026-07-08 20:49:45 -0500

    Added "hexify_float_array()" method to generate_coeffs
    hexify_float_array(arr_in, arr_name), converts a float array into an output file of one-val-per-line hex.

--- generate_coeffs.py
+++ generate_coeffs.py
@@ -5,34 +5,35 @@
 from scipy.signal import firwin
 from q115_conversions import float_to_q115
 
+# Constants
+fs = 25e6       # sampling frequency (Hz)
+fc = 1e6        # cutoff frequency (Hz)
+taps = 15       # number of taps (int)
+fir_coeff = firwin(taps, cutoff = fc, fs = fs)
 
-def make_q115_fir_coeffs(fs, fc, taps):
-    fir_coeff = firwin(taps, cutoff = fc, fs = fs)
 
+def hexify_float_array(float_array_in, array_name="input_array"):
+    q115_array = []
+    for i in float_array_in:
+        q115_array.append(float_to_q115(i))
 
-    fir_coeff_q115 = []
-    for i in fir_coeff:
-        fir_coeff_q115.append(float_to_q115(i))
-
-    return fir_coeff_q115
+    with open(f"{array_name}.hex", "w") as output_file_hex:
+        hex_str_array = []
+        for q115 in q115_array:
+            if q115 > 32768:
+                print("====================")
+                print("-----ERROR: bad Q1.15 value!-----\n" \
+                "This would've smoked your FPGA logic. " \
+                "   You should check your Q1.15 conversions")
+                print("====================")
+            else:
+                hex_str_array.append(f"{q115:0x}")
+        output_file_hex.write('\n'.join(hex_str_array))
+    return hex_str_array
 
 
 def main():
-    fs = 25e6       # sampling frequency (Hz)
-    fc = 1e6        # cutoff frequency (Hz)
-    taps = 15       # number of taps (int)
-
-    fir_coeff_q115 = make_q115_fir_coeffs(fs, fc, taps)
-
-    with open("fir_coeffs.hex", "w") as output_file_hex:
-        fir_coeff_hex = []
-        for coeff in fir_coeff_q115:
-            if coeff > 32768:
-                print("This would've smoked your FPGA logic. " \
-                "   You should check your Q1.15 conversions")
-            else:
-                fir_coeff_hex.append(f"{coeff:0x}")
-        output_file_hex.write('\n'.join(fir_coeff_hex))
+    hexify_float_array(fir_coeff, "fir_coeffs")
     
     # with open("fir_coeffs.hex") as f:
     #     print(f.read())
