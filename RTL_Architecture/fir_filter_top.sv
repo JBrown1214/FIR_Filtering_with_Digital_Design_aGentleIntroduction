2159c4f1413e8197895a0e9cc0e8bc12a93d79e7
Johnny <johnnybrown0g@gmail.com>
2026-07-26 13:09:44 -0500

    RTL: refactor FIRf verilog pipeline to 9-stage tree structure
    - Extend reset logic to cover all intermediate pipeline registers.
    - Align center-tap (x_reg[23]) latency through pre-adder layer.
    - Implement 5-stage binary reduction tree for 24-to-1 accumulator path.
    - Formally preserve 23-bit bit-growth (Q8.15) to guarantee zero overflow.
    - Yield valid FIR outputs continuously starting from the 9th clock cycle.

--- RTL_Architecture/fir_filter_top.sv
+++ RTL_Architecture/fir_filter_top.sv
@@ -1,33 +1,85 @@
 module fir_filter_top(
     input logic clk,
-    input logic rst_n,                  // rst"_n" means we are using active low reset
-    input logic signed [15:0] data_in 
+    input logic rst_n,                      // rst"_n" means we are using active low reset
+    input logic signed [15:0] data_in, 
+    output logic signed [22:0] FinalSum           // Q8.15 * 1
 );
-logic signed [15:0] x_reg [0:46];       // 47 value window, 47 "taps"
-logic signed [15:0] coeff_regs [0:23];
-logic signed [16:0] pre_add_regs [0:22];     // 17'b; holds 23 values (0-22, 24-46), index 23 is the center
-logic signed [32:0] product_regs [0:23];     // 33'b = (17'b sum) * (16'b Q1.15 float)     
+// 47 taps + 24 coefficients (1 coeff for each pair + 1 coeff for the middle value at index 23)
+logic signed [15:0] x_reg [0:46];           // Q1.15
+logic signed [15:0] coeff_regs [0:23];      // Q1.15
 
+// 23 17'b pre_add registers to store the result of the inital "halving" addition (extra bit is 2^0 overflow)
+logic signed [16:0] pre_add_regs [0:23];    // Q2.15: 1`sign + 1'overflow + 15'fracb
+
+// 24 33'b product registers to store the result of the float multiplication (pre-add * coeff)
+    // pre-add = 17'b: 1'sign + 1'overflow + 15'fracb
+    // coeff = 16'b: 1'sign + 15'fracb
+    // pre-add * coeff = 1'sign + 2' overflow + 30'fracb
+    // (2' of overflow is required in the case of -2 * -1 = 2)
+logic signed [33:0] product_regs [0:23];    // Q3.30: 1'signb + 2'intoverflow + 30'fracb)
+
+// 24 Right shift registers to store the result of the "rounding" Rshift operation (needed for pipelining)
+logic signed [17:0] Rshift_regs [0:23];     // Q3.15
+
+// registers for each clock cycle of the pipeline accumulator
+// we need to add an overflow bit for every addition to safeguard the theoretical worst case. 
+logic signed [18:0] Accum_reg_24to12 [0:11];// Q4.15 * 12
+logic signed [19:0] Accum_reg_12to6 [0:5];  // Q5.15 * 6
+logic signed [20:0] Accum_reg_6to3 [0:2];   // Q6.15 * 3
+logic signed [21:0] Accum_reg_3to2 [0:1];   // Q7.15 * 1 + Q6.15 * 1 (reg for both vals is needed for pipeline)
+//logic signed [22:0] FinalSum;         // Q8.15 * 1    (previous defined)
 
 always_ff @(posedge clk or negedge rst_n) begin : main
-    // Module Definition & Shift Register Array
-    if (!rst_n) begin                   // reset: everything goes to zero
-            for (int i = 0; i < 24; i++) coeff_regs[i] <= 16'sh0000;
+    
+    //* ==========Reset Logic==========
+    if (!rst_n) begin
             for (int i = 0; i < 47; i++) x_reg[i] <= 16'sh0000;
+            for (int i = 0; i < 24; i++) coeff_regs[i] <= 16'sh0000;
+            for (int i = 0; i < 24; i++) pre_add_regs[i] <= 16'sh0000;
+            for (int i = 0; i < 24; i++) product_regs[i] <= 16'sh0000;
+            for (int i = 0; i < 24; i++) Rshift_regs[i] <= 16'sh0000;
+            for (int i = 0; i < 12; i++) Accum_reg_24to12[i] <= 16'sh0000;
+            for (int i = 0; i < 6; i++) Accum_reg_12to6[i] <= 16'sh0000;
+            for (int i = 0; i < 3; i++) Accum_reg_6to3[i] <= 16'sh0000;
+            for (int i = 0; i < 2; i++) Accum_reg_3to2[i] <= 16'sh0000;
+            FinalSum <= 16'sh0000;
         end 
-    else begin                          // NOT reset, shift by one (take in new datapoint)
+    else begin
+        //* ==========data_in Shift Register Array (1 clock)==========
         x_reg[0] <= data_in;
-        for (int i = 1; i < 47; i++) begin
+        for (int i = 1; i < 47; i++) begin      // shift by one (take in new datapoint)
             x_reg[i] <= x_reg[i-1];
         end
-    end
 
-    // Pipelined Pre-Adder & Multiplier Layer
-    for (int i = 0; i <23; i++) begin
-        pre_add_regs[i] <= x_reg[i] + x_reg[46-i];
-        
-    end
+        //* ==========Pipelined Pre-Adder & Multiplier Layer (3 clocks)==========
+        for (int i = 0; i <23; i++) begin       // Pre-Adder      
+            pre_add_regs[i] <= x_reg[i] + x_reg[46-i];
+        end
+        pre_add_regs[23] <= x_reg[23]           // keep x[23] in-time with other x_reg values (not a clock behind)
 
+        for (int i = 0; i <24; i++) begin       // Multiplier      
+            product_regs[i] <= pre_add_regs[i] * coeff_regs[i];
+        end
+
+        for (int i = 0; i <24; i++) begin       // Right-shift      
+            Rshift_regs[i] <= product_regs[i] >>> 15;
+        end
+
+        //* ==========Pipelined Accumulator Layer (5 clocks)==========
+        // 24 values --clk1-> 12v --clk2-> 6v --clk3-> 3v --clk4-> 2v --clk5-> 1 value
+        for (int i = 0; i < 12; i++) begin
+            Accum_reg_24to12[i] <= Rshift_regs[i] + Rshift_regs[23-i];
+        end
+        for (int i = 0; i < 6; i++) begin
+            Accum_reg_12to6[i] <= Accum_reg_24to12[i] + Accum_reg_24to12[11-i];
+        end
+        for (int i = 0; i < 3; i++) begin
+            Accum_reg_6to3[i] <= Accum_reg_12to6[i] + Accum_reg_12to6[5-i];
+        end
+        Accum_reg_3to2[0] <= Accum_reg_6to3[0] + Accum_reg_6to3[1];
+        Accum_reg_3to2[1] <= Accum_reg_6to3[2];
+        FinalSum <= Accum_reg_3to2[0] + Accum_reg_3to2[1];
     end
+end
 
 endmodule
